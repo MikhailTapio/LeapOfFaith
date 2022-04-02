617a4d7f927bfd2bd44477d3c23f1c6521ec03eb
MikhailTapio <81358657+MikhailTapio@users.noreply.github.com>
2022-04-02 23:11:37 +0800

    Port to 1.8

---
+++ src/main/scala/committee/nova/lofaith/LeapOfFaith.scala
@@ -0,0 +1,27 @@
+package committee.nova.lofaith
+
+import committee.nova.lofaith.LeapOfFaith.proxy
+import committee.nova.lofaith.common.proxy.CommonProxy
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.{FMLInitializationEvent, FMLPreInitializationEvent}
+import net.minecraftforge.fml.common.{Mod, SidedProxy}
+
+object LeapOfFaith {
+  final val VERSION = "1.0"
+  final val MODID = "lofaith"
+  @SidedProxy(serverSide = "committee.nova.lofaith.common.proxy.CommonProxy")
+  val proxy: CommonProxy = new CommonProxy
+  @Mod.Instance(LeapOfFaith.MODID)
+  var instance: LeapOfFaith = _
+}
+
+@Mod(modid = LeapOfFaith.MODID, version = LeapOfFaith.VERSION)
+class LeapOfFaith {
+  LeapOfFaith.instance = this
+
+  @EventHandler
+  def preInit(event: FMLPreInitializationEvent): Unit = proxy.preInit(event)
+
+  @EventHandler
+  def init(event: FMLInitializationEvent): Unit = proxy.init(event)
+}
