c7b6acc1f22ca2d977694e9097841e7f7803b4f7
MikhailTapio <81358657+MikhailTapio@users.noreply.github.com>
2022-04-03 10:25:14 +0800

    1.9~1.10

---
+++ src/main/scala/committee/nova/lofaith/common/handler/FallHandler.scala
@@ -0,0 +1,26 @@
+package committee.nova.lofaith.common.handler
+
+import committee.nova.lofaith.common.config.CommonConfig
+import net.minecraft.util.math.BlockPos
+import net.minecraftforge.event.entity.living.LivingFallEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
+import scala.math.round
+
+class FallHandler {
+  @SubscribeEvent
+  def onLivingFallEvent(event: LivingFallEvent): Unit = {
+    val entity = event.getEntityLiving
+    if (entity == null) return
+    val world = entity.worldObj
+    if (world.isRemote) return
+    val pos = new BlockPos(round(entity.posX).intValue(), entity.posY, round(entity.posZ)).down()
+    val blocks = Array(
+      world.getBlockState(pos).getBlock,
+      world.getBlockState(pos.west()).getBlock,
+      world.getBlockState(pos.north()).getBlock,
+      world.getBlockState(pos.west().north()).getBlock
+    )
+    for (block <- blocks if CommonConfig.blocks.contains(block.getUnlocalizedName.substring(5))) event.setCanceled(true)
+  }
+}
